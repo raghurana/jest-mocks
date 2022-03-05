951f6913371e0c86272ba2ea72be947c2e70972e
Raghu Rana <raghurana@gmail.com>
2022-03-05 18:25:42 +1100

    example 1

--- src/program.ts
+++ src/program.ts
@@ -1,7 +1,13 @@
-export default class Program  {
-    static main(): void {
-        console.log('Hello world !!');
+export default class Program {
+  static main(): void {
+    console.log("Hello world !!");
+  }
+
+  static jestMockFunctions(items: number[], callback: Function) {
+    for (let index = 0; index < items.length; index++) {
+      callback(items[index]);
     }
+  }
 }
 
 Program.main();
--- tests/program.test.ts
+++ tests/program.test.ts
@@ -1,5 +1,21 @@
-import Program from '../src/program';
+import Program from "../src/program";
 
-test('Program is defined', () => {
-    expect(Program).toBeDefined();
-})
+test("Program is defined", () => {
+  expect(Program).toBeDefined();
+});
+
+// Testing using mock functions
+test("jestMockFunctions", () => {
+  const mockFn = jest.fn((num) => 42 + num);
+  Program.jestMockFunctions([100, 101], mockFn);
+
+  // Expect that the callback func was called twice, once for each number in the numbers array
+  expect(mockFn.mock.calls.length).toBe(2);
+
+  // Expect that the first call was called with arg 100 and second with 101
+  expect(mockFn.mock.calls[0][0]).toBe(100);
+  expect(mockFn.mock.calls[1][0]).toBe(101);
+
+  // Expect that the first call returned with a result 142
+  expect(mockFn.mock.results[0].value).toBe(142);
+});
