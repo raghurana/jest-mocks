7e6915c4befccf66de428fd609020060159710d1
Raghu Rana <raghurana@gmail.com>
2022-03-05 18:05:19 +1100

    Initial commit

---
+++ src/program.ts
@@ -0,0 +1,7 @@
+export default class Program  {
+    static main(): void {
+        console.log('Hello world !!');
+    }
+}
+
+Program.main();
---
+++ tests/program.test.ts
@@ -0,0 +1,5 @@
+import Program from '../src/program';
+
+test('Program is defined', () => {
+    expect(Program).toBeDefined();
+})
